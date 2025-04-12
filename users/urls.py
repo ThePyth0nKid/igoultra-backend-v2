acb30ac24da91d78c4e1208feab29a5fac0b64b2
Nelson <nelson.mehlis@icloud.com>
2025-04-12 11:40:21 +0200

    Refactor Discord login URL to simplify route structure

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from users.views import DiscordLogin
+from .views import DiscordLogin
 
 urlpatterns = [
-    # ... andere Routen ...
-    path("api/auth/social/discord/", DiscordLogin.as_view(), name="discord_login"),
+    path("discord/", DiscordLogin.as_view(), name="discord_login"),
 ]
